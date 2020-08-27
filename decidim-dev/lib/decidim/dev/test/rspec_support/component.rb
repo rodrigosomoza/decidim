bf8d9de7cde93caa8b830a793fa3d58e4931b572
Oriol Gual <oriol@codegram.com>
2020-08-27 18:04:59 +0200

    Add counter cache to comments

--- decidim-dev/lib/decidim/dev/test/rspec_support/component.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/component.rb
@@ -253,6 +253,7 @@ RSpec.configure do |config|
           t.datetime :published_at
           t.integer :coauthorships_count, null: false, default: 0
           t.integer :endorsements_count, null: false, default: 0
+          t.integer :comments_count, null: false, default: 0
 
           t.references :decidim_component, index: false
           t.integer :decidim_author_id, index: false
@@ -285,6 +286,7 @@ RSpec.configure do |config|
           t.datetime :published_at
           t.integer :coauthorships_count, null: false, default: 0
           t.integer :endorsements_count, null: false, default: 0
+          t.integer :comments_count, null: false, default: 0
 
           t.references :decidim_component, index: false
           t.references :decidim_category, index: false
