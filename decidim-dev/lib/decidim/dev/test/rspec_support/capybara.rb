3ce53560b3d7becfd79729befe8aea3cdc83e01f
Oriol Gual <oriol@codegram.com>
2020-09-02 18:00:43 +0200

    Add last comment metadata to debates

--- decidim-dev/lib/decidim/dev/test/rspec_support/capybara.rb
+++ decidim-dev/lib/decidim/dev/test/rspec_support/capybara.rb
@@ -23,7 +23,7 @@ Capybara.register_driver :headless_chrome do |app|
   options = ::Selenium::WebDriver::Chrome::Options.new
   options.args << "--headless"
   options.args << "--no-sandbox"
-  options.args << "--window-size=1024,768"
+  options.args << "--window-size=1920,3000"
 
   Capybara::Selenium::Driver.new(
     app,
